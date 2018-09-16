bdc3414640942af2b77fa1d7ab8fb3762fa04fcc
minwook-shin <minwook0106@gmail.com>
2018-09-16 19:48:20 +0900

    add kotlin extension function collection

--- app/src/main/java/io/github/teammoim/moim/common/ExtensionFunction.kt
+++ app/src/main/java/io/github/teammoim/moim/common/ExtensionFunction.kt
@@ -1,10 +1,230 @@
 package io.github.teammoim.moim.common
 
-import android.app.Activity
-import org.jetbrains.anko.longToast
+/**
+MIT License
 
-object ExtensionFunction{
-    fun Activity.toast(text : String){
-        longToast(text)
+Copyright (c) 2017 Ravindra Kumar
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.content.Intent.*
+import android.graphics.Bitmap
+import android.graphics.Matrix
+import android.graphics.Paint
+import android.graphics.Typeface
+import android.support.v4.app.Fragment
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import java.io.File
+import java.io.FileOutputStream
+
+/**
+ * Extension method to show a keyboard for View.
+ */
+fun View.showKeyboard() {
+    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    this.requestFocus()
+    imm.showSoftInput(this, 0)
+}
+
+/**
+ * Try to hide the keyboard and returns whether it worked
+ * https://stackoverflow.com/questions/1109022/close-hide-the-android-soft-keyboard
+ */
+fun View.hideKeyboard(): Boolean {
+    try {
+        val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        return inputMethodManager.hideSoftInputFromWindow(windowToken, 0)
+    } catch (ignored: RuntimeException) {
     }
+    return false
+}
+
+
+/**
+ * Set an onclick listener
+ */
+fun <T : View> T.click(block: (T) -> Unit) = setOnClickListener { block(it as T) }
+
+/**
+ * Extension method to set OnClickListener on a view.
+ */
+fun <T : View> T.longClick(block: (T) -> Boolean) = setOnLongClickListener { block(it as T) }
+
+/**
+ * Show the view  (visibility = View.VISIBLE)
+ */
+fun View.show(): View {
+    if (visibility != View.VISIBLE) {
+        visibility = View.VISIBLE
+    }
+    return this
+}
+
+/**
+ * Hide the view. (visibility = View.INVISIBLE)
+ */
+fun View.hide(): View {
+    if (visibility != View.INVISIBLE) {
+        visibility = View.INVISIBLE
+    }
+    return this
+}
+
+/**
+ * Remove the view (visibility = View.GONE)
+ */
+fun View.remove(): View {
+    if (visibility != View.GONE) {
+        visibility = View.GONE
+    }
+    return this
+}
+
+/**
+ * Toggle a view's visibility
+ */
+fun View.toggleVisibility(): View {
+    if (visibility == View.VISIBLE) {
+        visibility = View.INVISIBLE
+    } else {
+        visibility = View.INVISIBLE
+    }
+    return this
+}
+
+/**
+ * Extension method to share for Context.
+ */
+fun Context.share(text: String, subject: String = ""): Boolean {
+    val intent = Intent()
+    intent.type = "text/plain"
+    intent.putExtra(EXTRA_SUBJECT, subject)
+    intent.putExtra(EXTRA_TEXT, text)
+    try {
+        startActivity(createChooser(intent, null))
+        return true
+    } catch (e: ActivityNotFoundException) {
+        return false
+    }
+}
+
+/**
+ * Extension method to share text for Fragment.
+ */
+fun Fragment.share(text: String, subject: String = "") = activity!!.share(text, subject)
+
+
+/**
+ * Extension method to provide hide keyboard for [Fragment].
+ */
+fun Fragment.hideSoftKeyboard() {
+    activity?.hideSoftKeyboard()
+}
+
+fun Activity.hideSoftKeyboard() {
+    if (currentFocus != null) {
+        val inputMethodManager = getSystemService(Context
+                .INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+    }
+}
+
+/**
+ * Extension method underLine for TextView.
+ */
+fun TextView.underLine() {
+    paint.flags = paint.flags or Paint.UNDERLINE_TEXT_FLAG
+    paint.isAntiAlias = true
+}
+
+/**
+ * Extension method deleteLine for TextView.
+ */
+fun TextView.deleteLine() {
+    paint.flags = paint.flags or Paint.STRIKE_THRU_TEXT_FLAG
+    paint.isAntiAlias = true
+}
+
+/**
+ * Extension method bold for TextView.
+ */
+fun TextView.bold() {
+    paint.isFakeBoldText = true
+    paint.isAntiAlias = true
+}
+
+/**
+ * Extension method to set font for TextView.
+ */
+fun TextView.font(font: String) {
+    typeface = Typeface.createFromAsset(context.assets, "fonts/$font.ttf")
+}
+
+/**
+ * Extension method to load imageView from url.
+ */
+fun ImageView.loadFromUrl(imageUrl: String) {
+    Glide.with(this).load(imageUrl).into(this)
+}
+
+/**
+ * Extension method to get value from EditText.
+ */
+val EditText.value
+    get() = text.toString()
+
+/**
+ * Extension method to resize Bitmap to specified height and width.
+ */
+fun Bitmap.resize(w: Number, h: Number): Bitmap {
+    val width = width
+    val height = height
+    val scaleWidth = w.toFloat() / width
+    val scaleHeight = h.toFloat() / height
+    val matrix = Matrix()
+    matrix.postScale(scaleWidth, scaleHeight)
+    if (width > 0 && height > 0) {
+        return Bitmap.createBitmap(this, 0, 0, width, height, matrix, true)
+    }
+    return this
+}
+
+/**
+ * Extension method to save Bitmap to specified file path.
+ */
+fun Bitmap.saveFile(path: String) {
+    val f = File(path)
+    if (!f.exists()) {
+        f.createNewFile()
+    }
+    val stream = FileOutputStream(f)
+    compress(Bitmap.CompressFormat.PNG, 100, stream)
+    stream.flush()
+    stream.close()
 }
--- app/src/main/java/io/github/teammoim/moim/common/PermissionRequest.kt
+++
@@ -1,4 +0,0 @@
-package io.github.teammoim.moim.common
-
-object PermissionRequest{
-}
