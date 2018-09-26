68066d44ef66aa169cfa9756e7c1898a1e939bad
minwook-shin <minwook0106@gmail.com>
2018-09-26 18:55:59 +0900

    add friends fragment

--- app/src/main/java/io/github/teammoim/moim/view/fragment/FriendsFragment.kt
+++ app/src/main/java/io/github/teammoim/moim/view/fragment/FriendsFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import io.github.teammoim.moim.R
 import io.github.teammoim.moim.base.BaseFragment
 
+class FriendsFragment : BaseFragment(){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_friends,container,false)
     }
