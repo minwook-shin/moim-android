d5255240fa59a80fd75c311ca4a098afae5964a1
minwook-shin <minwook0106@gmail.com>
2018-10-05 22:05:09 +0900

    add information screen for ar

--- app/src/main/java/io/github/teammoim/moim/view/fragment/ARcameraFragment.kt
+++ app/src/main/java/io/github/teammoim/moim/view/fragment/ARcameraFragment.kt
@@ -15,17 +15,21 @@ import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.ux.ArFragment
 import io.github.teammoim.moim.App
 import org.jetbrains.anko.toast
+import com.google.ar.sceneform.rendering.ViewRenderable
+import com.google.ar.sceneform.math.Vector3
+
+
+
+
 
 
 class ARcameraFragment : BaseFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_ar_camera, container, false)
     }
-
     private var arFragment: ArFragment? = null
     private lateinit var andyRenderable: ModelRenderable
-
-
+    private lateinit var testViewRenderable: ViewRenderable
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupView()
@@ -38,17 +42,24 @@ class ARcameraFragment : BaseFragment() {
                     null
                 }
 
+        ViewRenderable.builder()
+                .setView(this.activity, R.layout.widget_ar)
+                .build()
+                .thenAccept { renderable ->testViewRenderable = renderable }
+
         arFragment?.setOnTapArPlaneListener { hitResult: HitResult, plane: Plane, motionEvent: MotionEvent ->
-            // Create the Anchor.
             val anchor = hitResult.createAnchor()
             val anchorNode = AnchorNode(anchor)
             anchorNode.setParent(arFragment?.arSceneView?.scene)
-
-            // Create the transformable andy and add it to the anchor.
             val andy = TransformableNode(arFragment?.transformationSystem)
             andy.setParent(anchorNode)
             andy.renderable = andyRenderable
             andy.select()
+            val widget = TransformableNode(arFragment?.transformationSystem)
+            widget.setParent(anchorNode)
+            widget.renderable = testViewRenderable
+            widget.localPosition = Vector3(0.0f, 0.25f, 0.0f)
+            widget.select()
         }
     }
 
