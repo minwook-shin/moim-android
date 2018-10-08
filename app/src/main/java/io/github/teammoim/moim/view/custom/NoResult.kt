491b7291c2f9769063c49d01489375c28f854b72
minwook-shin <minwook0106@gmail.com>
2018-10-08 14:56:24 +0900

    add no-result function using timeline

---
+++ app/src/main/java/io/github/teammoim/moim/view/custom/NoResult.kt
@@ -0,0 +1,13 @@
+package io.github.teammoim.moim.view.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import io.github.teammoim.moim.R
+import io.github.teammoim.moim.base.BaseView
+
+class NoResult @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : BaseView(context, attrs, defStyleAttr) {
+    init {
+        LayoutInflater.from(context).inflate(R.layout.custom_no_result_view, this, true)
+    }
+}
