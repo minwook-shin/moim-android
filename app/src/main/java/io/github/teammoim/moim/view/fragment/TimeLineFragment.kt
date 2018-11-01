e3a40885a4141129d3fb9e82b4951118a3a44266
minwook-shin <minwook0106@gmail.com>
2018-11-01 14:19:21 +0900

    add timeline firebase refresh

--- app/src/main/java/io/github/teammoim/moim/view/fragment/TimeLineFragment.kt
+++ app/src/main/java/io/github/teammoim/moim/view/fragment/TimeLineFragment.kt
@@ -24,8 +24,14 @@ import android.widget.EditText
 import com.blankj.utilcode.util.SnackbarUtils.dismiss
 import android.widget.ImageButton
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import io.github.teammoim.moim.common.remove
 import io.github.teammoim.moim.view.custom.NoResult
+import org.jetbrains.anko.longToast
+import java.util.*
 
 
 class TimeLineFragment : BaseFragment(), SwipeRefreshLayout.OnRefreshListener {
@@ -38,13 +44,42 @@ class TimeLineFragment : BaseFragment(), SwipeRefreshLayout.OnRefreshListener {
     private var timelineList: ArrayList<TimelineModel> = ArrayList()
 
     override fun onRefresh() {
-        addItem()
+        if (!App.INSTANCE.timelineArray.isEmpty()){
+            noResult.remove()
+        }
+        val cal = Calendar.getInstance()
+        FirebaseManager.getRef("timeline")?.addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onCancelled(p0: DatabaseError) {
+            }
+            override fun onDataChange(p0: DataSnapshot) {
+                timelineList.clear()
+                App.INSTANCE.timelineArray.clear()
+                for (snapshot in p0.children){
+
+                    val tmp = TimelineModel("","","")
+                    for (s in snapshot.children){
+                        if (s.key == "name"){
+                            tmp.name = s.value.toString()
+                        }
+                        if (s.key == "text"){
+                            tmp.text = s.value.toString()
+                        }
+
+                    }
+                    App.INSTANCE.timelineArray.add(tmp)
+                }
+
+                addItem()
+            }
+
+        })
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         swipe.setOnRefreshListener(this)
 
+        onRefresh()
         addItem()
 
         val mLayoutManager = LinearLayoutManager(this.activity)
@@ -57,6 +92,7 @@ class TimeLineFragment : BaseFragment(), SwipeRefreshLayout.OnRefreshListener {
     }
 
     fun addItem() {
+
         timeline_list.invalidate()
         timelineList = App.INSTANCE.timelineArray
         timeline_list.adapter?.notifyDataSetChanged()
